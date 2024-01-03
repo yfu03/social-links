f18258094aca7f1c5452549070ea00341c0b2847
yfu03 <yannisfu@gmail.com>
2024-01-02 21:51:29 -0600

    footer temp
    unfinished

---
+++ app/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <main>
+      <a>credit bg image from Koe - Vocaloid Cover Album https://www.pixiv.net/en/artworks/61829883</a>
+      <a>credit website ui inspiration from monko2k.xyz</a>
+    </main>
+  )
+}
