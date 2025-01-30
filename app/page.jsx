9d66e9c9067d3c13854ef5f072ef1b600f82fa19
yfu03 <yannisfu@gmail.com>
2025-01-29 18:39:23 -0600

    gets pfp from osu site now

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,27 @@
 import Image from "next/image";
-import osudefaultpfp from './components/osudefaultpfp.png'
+
+let osupfp = "https://a.ppy.sh/8525223";
 
 export default function Home() {
   return (
     <main>
-      <Image 
-        src={osudefaultpfp}
-        alt="osu default"
-        width={500}
-        quality={10}
-        placeholder='blur'
-      />
+      <div>
+        <Image 
+          src={osupfp}
+          alt="osu default"
+          width={400}
+          height={400}
+          quality={10}
+          className='rounded-3xl aspect-square object-cover'
+          //placeholder='blur' //need to convert to data URL to be able to use placeholder
+          loading="eager" //test with "eager" later
+        />
+      </div>
       <h1>whats up</h1>
-      <iframe src="https://osekai.net/profiles/img/banner.svg?id=8525223"></iframe>
+      <iframe 
+      width="1000"
+      height="200"
+      src="https://osekai.net/profiles/img/banner.svg?id=8525223"></iframe>
     </main>
   )
 }
